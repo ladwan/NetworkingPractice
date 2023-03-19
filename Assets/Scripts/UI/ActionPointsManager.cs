f93146abe64e2c416ae420ab957e7c3d77121a24
Lj <ladwanhester@gmail.com>
2023-03-18 22:15:25 -0400

    Working movement between cubes// Push before editing turn ending on confirm move

--- Assets/Scripts/UI/ActionPointsManager.cs
+++ Assets/Scripts/UI/ActionPointsManager.cs
@@ -154,9 +154,4 @@ namespace ForverFight.Ui
             blinkCoroutineIsRunning = false;
         }
     }
-
-
-    //TODO: Put finishing touches on AP movement system
-    // a) Stop ending turn after confirmed move.
-    // b) Fix any Ui oddities that may arrise with the switching of thw movement systems
 }
---
+++ Assets/ToDo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDo : MonoBehaviour
+{
+    /*
+        TODO: Put finishing touches on AP movement system
+
+            a) Stop ending turn after confirmed move.
+            b) Fix any Ui oddities that may arrise with the switching of thw movement systems 
+    */
+}
