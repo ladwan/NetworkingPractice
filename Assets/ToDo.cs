8b149ed8de2e25187548ea8295bfad3eebb87ceb
Lj <ladwanhester@gmail.com>
2023-04-30 13:00:46 -0400

    Clean commit before implementing dynamic camera

--- Assets/ToDo.cs
+++ Assets/ToDo.cs
@@ -7,11 +7,6 @@ public class ToDo : MonoBehaviour
     /*
         TODO: Put finishing touches on AP movement system
             
-          ***  a) Figure out why after one turn the apLights stop working
-          ***  b) Stop ending turn after confirmed move.
-           *** c) Fix any Ui oddities that may arrise with the switching of the movement systems
-           *** d) Implement all the above changes for player 2
-          ***  e) Fix die image on ui  (its stretched) 
-            ***f) Try to recreate movement bug with highlights not resetting when you go over the same sq properly
+            a) Add end turn button
     */
 }
